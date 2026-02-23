8b2129b7888e2333bf1429ae6a4e74dc26743dc2
Zbigniew <zbigniewszulc@arachas.ie>
2026-02-23 15:31:34 +0000

    Move FileResults.cs to Models folder

---
+++ Models/FileResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Models
+{
+    public class FileResult
+    {
+        public string FilePath { get; set; } = string.Empty;
+        public DateTime LastModified { get; set; }
+        public string Owner {  get; set; } = string.Empty ;
+    }
+}
