01728055f8a07d7008b841715633b483ff914268
Zbigniew <zbigniewszulc@arachas.ie>
2026-02-23 14:27:12 +0000

    Add Model

---
+++ Model/FileResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Model
+{
+    internal class FileResult
+    {
+        public string FilePath { get; set; }
+        public DateTime LastModified { get; set; }
+        public string Owner {  get; set; }
+    }
+}
