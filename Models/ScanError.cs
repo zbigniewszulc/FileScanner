2901af2195c4aae60980667278c9ffd81e8e70e6
Zbigniew Szulc <zbigniewszulc@gmail.com>
2026-03-01 17:38:00 +0000

    Add ScanError model

---
+++ Models/ScanError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.Models
+{
+    internal class ScanError
+    {
+        public string Path { get; set; } = string.Empty;
+        public string Reason { get; set; } = string.Empty;
+    }
+}
